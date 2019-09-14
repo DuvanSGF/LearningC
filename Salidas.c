719f41d67894446cd3bdd5678fb309a41caa8cda
DuvanSGF <software.bugs.io@gmail.com>
2019-09-13 21:50:46 -0500

    Entradas y Salidas

---
+++ Salidas.c
@@ -0,0 +1,15 @@
+// Entradas  y salidas
+
+#include<stdio.h>
+
+int main(){
+	char x [50];
+	
+	printf("Digite su nombre: ");
+	gets(x);// Para guardar todo el nombre
+	
+	printf("Su nombre es : %s", x);
+	
+	
+	return 0;
+}
